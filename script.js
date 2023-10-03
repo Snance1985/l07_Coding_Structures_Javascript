76ee8a3a64b2f61328886a132813784529db8c72
Sean Nance <sean_nance@hotmail.com>
2023-10-02 22:41:14 -0700

    created index.html style.css and script.js files

---
+++ script.js
@@ -0,0 +1,82 @@
+//console.log("Hello World!");
+
+//var message = "Hello Everyone";
+//console.log(message)
+
+//var numOne = 12;
+//var numTwo = 14;
+//var sum = numOne + numTwo;
+//console.log(sum);
+
+//var job = "Store Clerk";
+//var yearsOfService = "3";
+//var jobYears = "I have been a " + job + " for " + yearsOfService + " years.";
+//console.log(jobYears);
+
+//var addNumbers = 2 + 2;
+//console.log(addNumbers);
+// result will be: 4, the numbers added together
+
+//var concatStrings = "2" + "2";
+//console.log(concatStrings);
+// result will be: 22, not added together but put side by side
+
+//var yearsOfService = 5;
+//var qualifiedForManager = false;  
+
+//if (yearsOfService >= 3) {
+//    qualifiedForManager = true;
+//    console.log(qualifiedForManager, "you qualify for manager!");
+//}
+
+var yearsWorked = 2;
+var qualifiedForBonus = false;
+
+if (yearsWorked >= 3) {
+    qualifiedForBonus = true;
+    //console.log(qualifiedForBonus, "Congratulations! You qualify for a bonus!");
+} else if (yearsWorked == 2) {
+    //console.log("One more year and you'll get your bonus!");
+} else {
+    //console.log("Keep working to get that bonus!");
+}
+
+
+
+// Multiple "if" statements
+// First "if" statement
+var daysTilXmas = 2;
+var openPresents = false;
+
+if(daysTilXmas <= 2){
+    openPresents = true; 
+} else if (daysTilXmas == 3) {
+    //console.log("One more day until presents");
+}else {
+   // console.log("Don't even think about presents yet!");
+}
+
+// Second "if" statement
+if(openPresents){
+    //console.log("Time to open presents!")
+}
+
+var books = ["When Heaven Invades Earth", "Keep Your Love On", "Supernatural Ways of Royalty"];
+books.push("Heavy Rain");
+//console.log(books.length);
+//console.log(books);
+books.pop();
+//console.log(books.length);
+//console.log(books);
+//Add the code below
+books.sort();
+//console.log(books);
+
+
+//var lunch = ["PB&J", "apple", "chips", "brownie", "milk" ];
+
+//for (var i=0; i < lunch.length; i++) {
+    //console.log(lunch[i]);
+//}
+
+
